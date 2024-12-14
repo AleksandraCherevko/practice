a095584665761b0fb69aa311338429a3c7c18374
AleksandraCherevko <119119431+AleksandraCherevko@users.noreply.github.com>
2024-12-14 17:47:08 +0100

    Update objects.js

--- js/objects.js
+++ js/objects.js
@@ -624,7 +624,87 @@
 // };
 // console.log(thirdObj);
 
-function foo(...args) {
-  console.log(args);
-}
-foo(1, 2, 3, 4, 5);
+// function foo(...args) {
+//   console.log(args);
+// }
+// foo(1, 2, 3, 4, 5);
+
+// function isEnoughCapacity(products, containerSize) {
+//   let productValues = Object.values(products);
+
+//   let totalPorductValues = 0;
+//   for (let productValue of productValues) {
+//     totalPorductValues += productValue;
+//   }
+
+//   if (totalPorductValues <= containerSize) {
+//     return "true";
+//   }
+
+//   return "false";
+// }
+
+// console.log(isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8));
+// console.log(isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12));
+// console.log(isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14));
+
+// function calcAverageCalories(days) {
+//   let totalCalories = 0;
+
+//   for (let day of days) {
+//     totalCalories += day.calories;
+//   }
+//   const averageCalories = totalCalories / days.length;
+//   return averageCalories;
+// }
+
+// console.log(
+//   calcAverageCalories([
+//     { day: "monday", calories: 3010 },
+//     { day: "tuesday", calories: 3200 },
+//     { day: "wednesday", calories: 3120 },
+//     { day: "thursday", calories: 2900 },
+//     { day: "friday", calories: 3450 },
+//     { day: "saturday", calories: 3280 },
+//     { day: "sunday", calories: 3300 },
+//   ])
+// ); // 3180
+
+// console.log(
+//   calcAverageCalories([
+//     { day: "monday", calories: 2040 },
+//     { day: "tuesday", calories: 2270 },
+//     { day: "wednesday", calories: 2420 },
+//     { day: "thursday", calories: 1900 },
+//     { day: "friday", calories: 2370 },
+//     { day: "saturday", calories: 2280 },
+//     { day: "sunday", calories: 2610 },
+//   ])
+// ); // 2270
+
+// console.log(calcAverageCalories([])); // 0
+
+const profile = {
+  username: "Jacob",
+  playTime: 300,
+
+  getInfo() {
+    return `${this.username} has ${this.playTime} active hours!`;
+  },
+
+  changeUsername(newName) {
+    profile.username = newName;
+  },
+
+  // updatePlayTime(hours) {
+  //   for (let hour of hours) {
+  //     profile.playTime += hour;
+  //   }
+  // },
+};
+
+console.log(profile.getInfo());
+profile.changeUsername("Marco");
+console.log(profile.getInfo());
+// profile.updatePlayTime(20);
+// console.log(profile.getInfo());
