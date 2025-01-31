92d8df3658e610a98bc8524f81c9aabd095ad4f3
AleksandraCherevko <119119431+AleksandraCherevko@users.noreply.github.com>
2025-01-31 20:23:10 +0100

    upd

---
+++ js/module-seven.js
@@ -0,0 +1,5 @@
+const textEl = document.querySelector(".article-text");
+console.log(textEl.textContent); // text inside p.article-text
+
+const titleEl = document.querySelector(".article-title");
+titleEl.textContent = "Welcome to Bahamas!";
