8199459ad45d21b4b2c1ec6ad41cac0871977758
AleksandraCherevko <119119431+AleksandraCherevko@users.noreply.github.com>
2025-02-02 17:46:30 +0100

    update

--- js/module-seven.js
+++ js/module-seven.js
@@ -1,6 +1,49 @@
-// const textEl = document.querySelector(".article-text");
-// console.log(textEl.textContent); // text inside p.article-text
-
 // const titleEl = document.querySelector(".article-title");
 // titleEl.textContent = "Welcome to Bahamas!";
 
+// const heading = document.createElement("h1");
+// heading.classList.add("title");
+// heading.textContent = "it is title";
+// console.log(heading);
+
+// const list = document.querySelector(".usernames");
+// console.log(list);
+
+// const firstItem = document.createElement("li");
+// firstItem.textContent = "grape";
+// list.prepend(firstItem);
+
+// const lastItem = document.createElement("li");
+// lastItem.textContent = "poly";
+// list.append(lastItem);
+
+// const thirdItem = document.createElement("li");
+// thirdItem.textContent = "orange";
+// list.prepend(thirdItem);
+
+//  INNER HTML
+// const technologies = ["HTML", "CSS", "JavaScript", "React", "Node"];
+// const list = document.querySelector(".list");
+
+// const markup = technologies
+//   .map((technology) => `<li class="list-item">${technology}</li>`)
+//   .join("");
+
+// console.log(markup);
+// list.innerHTML = markup;
+
+// INSERTADJACENTHTML
+
+// const list = document.querySelector(".list");
+// const newTechnologies = ["React", "TypeScript", "Node.js"];
+// const markup = newTechnologies
+//   .map((technology) => `<li class="list-item new">${technology}</li>`)
+//   .join("");
+
+// list.insertAdjacentHTML("beforeend", markup);
+// list.insertAdjacentHTML("beforebegin", "<h2>Popular technologies</h2>");
+
+// const button = document.querySelector(".my-button ");
+// button.addEventListener("click", () => {
+//   console.log("Кнопка была нажата, и сейчас появится следующее изображение");
+// });
